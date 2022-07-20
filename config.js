2719860318a5e6b322c748c2a0ad4f88248261f2
ERGISTOREBOT <109357046+ERGISTOREBOT@users.noreply.github.com>
2022-07-20 10:17:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ global.urlnya = "https://www.instagram.com/aguzfamilia"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 822-6431-0024'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
